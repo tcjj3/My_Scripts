0c35f53e292665ac68b274254647303368b442ed
Chaojun Tan <495825947@qq.com>
2021-04-15 00:15:31 +0800

    Add files via upload

---
+++ Kali Linux/goesrecv-monitor__run.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+
+
+
+# Install wine32 & ".Net 4.8":
+apt-get install -y wine wine64
+dpkg --add-architecture i386 && apt-get update && apt-get install -y wine32
+apt-get install -y winetricks
+winetricks dotnet48
+
+
+
+
+# Download goesrecv-monitor:
+apt-get install -y unzip
+mkdir goestools-monitor
+curl -L https://github.com/sam210723/goesrecv-monitor/releases/latest/download/goesrecv-monitor.zip -o goestools-monitor/goesrecv-monitor.zip
+unzip -o goestools-monitor/goesrecv-monitor.zip -d goestools-monitor
+
+
+
+
+# Run goesrecv-monitor:
+wine goestools-monitor/goesrecv-monitor.exe
+
+
+
+
+
+
+
+
+
+
