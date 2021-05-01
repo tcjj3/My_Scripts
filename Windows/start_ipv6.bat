d88b8d5f767fc3dbe384cfb164b2ccf4750d8dd3
Chaojun Tan <495825947@qq.com>
2021-05-01 18:22:44 +0800

    Add files via upload

---
+++ Windows/start_ipv6.bat
@@ -0,0 +1,23 @@
+@echo off
+
+
+
+
+
+netsh int ter set state client
+netsh int teredo set state client teredo.remlab.net
+
+
+
+
+
+
+REM netsh int ipv6 show teredo state
+
+
+
+
+
+
+
+
---
+++ Windows/stop_ipv6.bat
@@ -0,0 +1,28 @@
+@echo off
+
+
+
+
+
+REM netsh int ter set state natawareclient
+REM netsh int teredo set state natawareclient win10.ipv6.microsoft.com.
+
+
+
+netsh int ter set state natawareclient
+netsh int teredo set state natawareclient win1901.ipv6.microsoft.com.
+
+
+
+
+
+
+REM netsh int ipv6 show teredo state
+
+
+
+
+
+
+
+
