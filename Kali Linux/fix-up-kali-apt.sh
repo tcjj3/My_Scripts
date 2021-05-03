550c3c315738c0b1d023d89ecd1128391fd0e973
Chaojun Tan <495825947@qq.com>
2021-05-03 14:25:50 +0800

    Add files via upload

--- Kali Linux/fix-up-kali-apt.sh
+++ Kali Linux/fix-up-kali-apt.sh
@@ -3,6 +3,16 @@
 
 
 
+
+
+# Update Date & Time to avoid "Release file for xxxxxx is not vaild yet (invaild for another xxxd xxh xxmin xxs)" error:
+ntpdate time.windows.com
+
+
+
+
+
+
 #curl -fsSL "http://kali.download/kali/dists/kali-rolling/Release.gpg" | apt-key add -qq - >/dev/null 2>&1
 
 #apt-get update --allow-insecure-repositories
