ab167cc2a84edb5f1c6015b821391f789939384d
Chaojun Tan <495825947@qq.com>
2021-05-19 14:48:59 +0800

    Add files via upload

--- Kali Linux/docker/docker_after_install.sh
+++ Kali Linux/docker/docker_after_install.sh
@@ -25,9 +25,14 @@ mv /etc/rc5.d/S01docker /etc/rc5.d/S99docker > /dev/null 2>&1
 
 
 # Copy and set up docker_pre_start:
-BASEDIR=$(dirname $0)
-cp $BASEDIR/docker_pre_start.sh /etc/init.d/docker_pre_start.sh > /dev/null 2>&1
+BASEDIR="$(dirname $0)"
+
+while [ ! -f "$BASEDIR/docker_pre_start.sh" ]; do
+curl -L https://github.com/tcjj3/My_Scripts/raw/main/Kali%20Linux/docker/docker_pre_start.sh -o "$BASEDIR/docker_pre_start.sh"
+done
+cp "$BASEDIR/docker_pre_start.sh" /etc/init.d/docker_pre_start.sh > /dev/null 2>&1
 chmod +x /etc/init.d/docker_pre_start.sh > /dev/null 2>&1
+
 
 ln -s /etc/init.d/docker_pre_start.sh /etc/rc2.d/S01docker_pre_start > /dev/null 2>&1
 ln -s /etc/init.d/docker_pre_start.sh /etc/rc3.d/S01docker_pre_start > /dev/null 2>&1
